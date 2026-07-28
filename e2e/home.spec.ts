2bae9b897b48b90ed56e4df1fce53f048086b975
ddg <thewhiteless@yahoo.com>
2026-07-29 00:12:44 +0300

    fix: remove comments and replace boilerplate test with app-specific test case

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    await page.goto("http://localhost:3000");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+    await expect(page).toHaveTitle(/Kanban-board/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has text content', async ({ page }) => {
+    await page.goto("http://localhost:3000");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await expect(
+        page.getByRole(
+            'heading',
+            { name: "INITIALIZE PROJECT AND CLEAN UP" }
+        )
+    ).toBeVisible();
 });
