0d36892f1e619d10331e8fabc77b27f091cb81ce
ddg <thewhiteless@yahoo.com>
2026-08-14 13:17:43 +0300

    feat: add test case to List component, start testing Board, destructure tests in folders

---
+++ __tests__/unit/components/board.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+
+// beforeEach(() => {
+//     render(<Board 
+
+//     />)
+// }); 
+
+describe("Board component", () => {
+    test("renders board with proper content", () => {
+
+    })
+});
