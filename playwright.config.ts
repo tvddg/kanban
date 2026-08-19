b47c9b2d6a1f99fd5eb90fcc04dcf5619e182dd1
ddg <thewhiteless@yahoo.com>
2026-08-19 15:54:07 +0300

    fix: return artifacts, set up the reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI 
+    ? [['github'], ['html', { open: 'never' }]]
+    : [['list'], ['html']],
   use: {
     screenshot: "only-on-failure",
     trace: 'on-first-retry',
